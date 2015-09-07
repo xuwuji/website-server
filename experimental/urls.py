5d333360990cc1dd972885346aaf5a508a7b833a
Jameslily <xuwuji@192.168.1.102>
2015-09-08 00:46:48 +0800

    update-9-7

--- experimental/urls.py
+++ experimental/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     url(r'^detail$', views.detail, name='detail'),
     url(r'^experimental_detail/(?P<place_name>sanya)/$', views.experimental_detail, name='experimental_detail_sanya'),
     url(r'^experimental_detail/(?P<place_name>roma)/$', views.experimental_detail, name='experimental_detail_roma'),
-    url(r'^experimental_detail/copenhagen', views.experimental_detail_copenhagen, name='experimental_detail_copenhagen'),
-    url(r'^experimental_detail/milan', views.experimental_detail_milan, name='experimental_detail_milan'),
-    url(r'^experimental_detail/esbjerg', views.experimental_detail_esbjerg, name='experimental_detail_esbjerg'),
-    url(r'^experimental_detail/barcelona', views.experimental_detail_barcelona, name='experimental_detail_barcelona'),
+    url(r'^experimental_detail/(?P<place_name>copenhagen)/$', views.experimental_detail, name='experimental_detail_copenhagen'),
+    url(r'^experimental_detail/(?P<place_name>milan)/$', views.experimental_detail, name='experimental_detail_milan'),
+    url(r'^experimental_detail/(?P<place_name>esbjerg)/$', views.experimental_detail, name='experimental_detail_esbjerg'),
+    url(r'^experimental_detail/(?P<place_name>barcelona)/$', views.experimental_detail, name='experimental_detail_barcelona'),
                 
 ]
--- xuwuji/urls.py
+++ xuwuji/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-
+from django.conf.urls.static import static
 
         
 urlpatterns = [
@@ -28,5 +28,4 @@ urlpatterns = [
 
 ]
 
-if not settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT )
