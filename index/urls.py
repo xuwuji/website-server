caf1f86c8abe5f744558b27a99ab6e484cc4cc64
Jameslily <xuwuji@192.168.1.101>
2015-11-28 09:38:22 +0800

    update

--- index/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic import RedirectView
-
-urlpatterns = [
-##the pattern is followed by the url pattern in urls.py in mysite
-    url(r'^$', views.index, name='index'),
-]
--- index/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-# Create your views here.
-def index(request):
-    return render(request, 'index/index.html',{})
